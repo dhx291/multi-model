d33dcfab0cc122df72690bdf4e79b1c03d7c1fd7
Duan <2912972039@qq.com>
2025-03-02 00:10:10 +0800

    解决路径别名报错问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
-import Page1 from './pages/Page1';
-import Page2 from './pages/Page2';
-import Page3 from './pages/Page3';
+import Page1 from '@/pages/page1';
+import Page2 from '@/pages/page2';
+import Page3 from '@/pages/page3';
 
 function App() {
   return (
